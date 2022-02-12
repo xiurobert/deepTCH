fbc78e7bcbc77a864c8960305682f46378ad1278
Robert <xiurobert@gmail.com>
2022-02-12 20:41:12 +0800

    Quick commit. I apparently did the DOT wrongly oops

--- src/dtypes.rs
+++ src/dtypes.rs
@@ -20,6 +20,13 @@ pub struct Matrix {
     data: Vec<Vec<f64>>,
 }
 
+impl Iterator for Matrix {
+    type Item = Vec<f64>;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.data.pop()
+    }
+}
+
 impl Matrix {
     /// Creates a new matrix from a Vec of Vecs.
     /// This checks the data for validity and returns an error if it is invalid
@@ -38,6 +45,12 @@ impl Matrix {
     /// ```
     pub fn new(data: Vec<Vec<f64>>) -> Result<Matrix, String> {
         let rows = data.len();
+        // if rows == 1 {
+        //     warn!("Matrix::new: Matrix is 1 row long. You probably want a Vec<f64> instead");
+        // }
+        // if cols == 0 {
+        //     return Err(format!("Matrix::new: Matrix is 0 columns long"));
+        // }
         let cols = data[0].len();
         for row in data.iter() {
             if row.len() != cols {
@@ -79,7 +92,7 @@ impl Matrix {
     /// vec![139.0, 154.0]
     /// ]);
     ///
-    pub fn dot(&self, other: Matrix) -> Result<Matrix, String> {
+    pub fn dot(&self, other: &Matrix) -> Result<Matrix, String> {
         if self.rows != other.cols || self.cols != other.rows {
             return Err(
                 format!("Cannot dot a [{} by {}] matrix with a [{} by {}] matrix (Shape error)",
@@ -88,14 +101,32 @@ impl Matrix {
                                other.rows,
                                other.cols));
         }
-        Ok(Matrix {
-            rows: self.rows,
-            cols: other.cols,
-            data: vec![vec![0.0; other.cols]; self.rows],
-        })
+        let mut result = Matrix::new_empty(self.rows, other.cols);
+        // We know that the other matrix is already transposed.
+        // So therefore the original cols are just the rows
+        for i in 0..self.rows {
+            // iterate through every row in the initial matrix, and just take the dot product.
+            let val_result = dot_product(&self.data[i], &other.data[i]);
+        }
+        Ok(result)
 
     }
 
+    /// Evaluates the dot product of this matrix and a vector
+    /// If a matrix with n rows and m columns is multiplied by a vector with m elements,
+    /// the result will be a vector with n elements (a n x 1 matrix)
+    /// # Example
+    /// ```
+    /// use dtypes::Matrix;
+    /// let m1 = Matrix::new(vec![
+    /// vec![1.0, 2.0, 3.0],
+    /// vec![4.0, 5.0, 6.0],
+    /// ]);
+    /// let v1 = vec![7.0, 8.0, 9.0];
+    /// let m2 = m1.dot_vector(&v1);
+    /// assert_eq!(m2.rows, 2);
+    /// assert_eq!(m2.cols, 1);
+    /// assert_eq!(m2.data, vec![50, 122]);
     pub fn dot_vector(&self, other: Vec<f64>) {
 
     }
@@ -148,7 +179,7 @@ impl fmt::Debug for Matrix {
 /// let b = vec![4.0, 5.0, 6.0];
 /// let c = dot_product(&a, &b);
 ///
-pub fn dot_product(a: Vec<f64>, b: Vec<f64>) -> f64 {
+pub fn dot_product(a: &[f64], b: &[f64]) -> f64 {
     if a.len() != b.len() {
         panic!("Vectors must be the same length to actually take a dot product");
     }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use crate::dtypes::Matrix;
 mod dtypes;
 
 fn main() {
+    let a = vec![1.0, 2.0, 3.0];
+    let b = vec![4.0, 5.0, 6.0];
     println!("Dot product of 1 2 3 and 4 5 6: {}",
-             dtypes::dot_product(vec![1.0, 2.0, 3.0], vec![4.0, 5.0, 6.0]));
+             dtypes::dot_product(&a, &b));
     let m = Matrix::new(vec![
         vec![1.0, 2.0, 3.0],
         vec![4.0, 5.0, 6.0]]).unwrap();
@@ -14,5 +16,8 @@ fn main() {
         vec![3.0, 6.0]]).unwrap();
     println!("Matrix: {:?}", &m);
     assert_eq!(m_transposed, dtypes::Matrix::transpose(&m));
-
+    println!("Transposed: {:?}", &m_transposed);
+    // Now attempt to dot them
+    println!("Dot product of m and m_transposed: {:?}",
+             dtypes::Matrix::dot(&m, &m_transposed));
 }
